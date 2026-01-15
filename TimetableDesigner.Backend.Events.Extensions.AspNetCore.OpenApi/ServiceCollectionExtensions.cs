c1a50c4f65dc9356cee21d132d3707b6f6238b7f
Mateusz Skoczek <mateusz5432160@gmail.com>
2026-01-15 23:47:04 +0100

    initial commit

---
+++ TimetableDesigner.Backend.Events.Extensions.AspNetCore.OpenApi/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TimetableDesigner.Backend.Events.Extensions.AspNetCore.OpenApi;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddEventQueue<T>(this IServiceCollection services, Action<T> configuration) where T : EventQueue, new()
+    {
+        T builder = new T();
+        configuration(builder);
+        builder.Setup(services);
+        return services;
+    }
+}
---
+++ TimetableDesigner.Backend.Events.Extensions.AspNetCore.OpenApi/WebApplicationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TimetableDesigner.Backend.Events.Extensions.AspNetCore.OpenApi;
+
+public static class WebApplicationExtensions
+{
+    public static void AddEventHandler<T>(this WebApplication app, Func<T, Task> handler) where T : class
+    {
+        IEventQueueSubscriber subscriber = app.Services.GetService<IEventQueueSubscriber>()!;
+        subscriber.Subscribe(handler);
+    }
+}
